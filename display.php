6f13c4bc358fd7544ea045d928b80eefd8e15038
Louis DEVIE <louisdevie.contact@gmail.com>
2023-04-14 13:28:05 +0200

    Déplacement de la vue des items dans un fichier séparé

--- display.php
+++ display.php
@@ -173,65 +173,15 @@ foreach ($formData as $name => $value) $baseActionUrl .= "&$name=$value";
 	<a href='edit.php<?= $noResultsUrl ?>'> <?= $l_msg_addsome ?></a>
 </div>
 
-<?php else: foreach ($result as $item): 
+<?php else: 
 
-if ($item->done()) $divClass = 'np';
-else if ($item->dueToday()) $divClass = 'current';
-else if ($item->overdue()) $divClass = 'old';
-else $divClass = 'np';
-
-if (!is_null($item->date())) $displayedDate = $item->date()->format(l->dateFormat->task);
-else $displayedDate = '';
-
-if ($item->done())
+foreach ($result as $item)
 {
-	$action = 'undo';
-	$actionIcon = 'accept.png';
-}
-else
-{
-	$action = 'do';
-	$actionIcon = 'undone.png';
+	include 'includes/item.php';
 }
 
-?>
+endif;
 
-<div class='<?= $divClass ?>'>
-
-	<span <?= $item->done() ? 'style="text-decoration:line-through;"' : '' ?> >
-		<?= htmlspecialchars($item->title()) ?>
-		<?= $displayedDate ?>
-		|
-		<?= htmlspecialchars($item->project()->title()) ?>
-		|
-		<?= htmlspecialchars($item->context()->title()) ?>
-	</span>
-
-	<a href='display.php?sort=<?= $sortBy . $baseActionUrl ?>&cmd=delete&id=<?= $item->id() ?>'
-		title='<?= l->items->delete ?>'
-		class='actionicon'>
-		<img src='images/bin_empty.png' alt='<?= l->items->del ?>' />
-	</a>
-	<a href='edit.php?id=<?= $item->id() ?>' title='<?= l->items->edit ?>' class='actionicon'>
-		<img src='images/pencil.png' alt='<?= l->items->edit ?>' />
-	</a> 
-	<a href='display.php?sort=<?= $sortBy . $baseActionUrl ?>&cmd=<?= $action ?>&id=<?= $item->id() ?>'
-		title='<?= l->items->$action ?>' class='actionicon'>
-		<img src='images/<?= $actionIcon ?>' alt='<?= l->items->$action ?>' />
-	</a>
-
-	<br/>
-
-	<?= htmlspecialchars($item->notes()) ?>
-
-	<br/>
-
-	<a href='<?= addslashes($item->url()) ?>' class='short-url'> <?= htmlspecialchars($item->url()) ?> </a>
-	
-</div>
-
-<?php endforeach; endif; 
-
-include('includes/footer.php');
+include 'includes/footer.php';
 
 ?>
