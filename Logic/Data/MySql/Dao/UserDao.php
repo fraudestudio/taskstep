977f08be25321c509316dcf30c441e8e7e2602e2
AlexandreANSTETT <alexandre.anstett@iut-dijon.u-bourgogne.fr>
2023-04-24 11:04:31 +0200

    Implémentation de SignUp

--- Logic/Data/MySql/Dao/UserDao.php
+++ Logic/Data/MySql/Dao/UserDao.php
@@ -1,13 +1,14 @@
 <?php
 
-declare(strict_types=1);
+namespace TaskStep\Data\MySql\Dao;
 
-namespace TaskStep\Logic\Model;
+use TaskStep\Data\Database;
+use TaskStep\Logic\Model;
+use TaskStep\Logic\Model\User;
+use TaskStep\Logic\Model\UserDaoInterface;
+use PDO;
 
-/**
- * Un DAO des projets.
- */
-interface UserDaoInterface
+class UserDAO implements UserDaoInterface
 {
     /**
      * Crée un utilisateur.
@@ -19,7 +20,13 @@ interface UserDaoInterface
      * 
      * @return int id duy nouvel utilisateur
      */ 
-    public function SignUp(string $login, string $password, string $mail) : int;
+    public function SignUp(string $login, string $password, string $mail) : int
+    {
+        $request = "Insert into `User`(login,MDP,salt,mail,style,tips) values (:login,:mdp,'SELTAMERE',:mail,0,1);";
+        Database::getInstance()->executeNonQuery($request,array('login'=>htmlspecialchars($login),'mdp'=>password_hash($password,PASSWORD_BCRYPT),'mail'=>$mail));
+        $data = Database::getInstance()->executeQuery("select last_insert_id();")->fetch(PDO::FETCH_ASSOC);
+        return $data['id'];
+    }
 
     /**
      * Récupère un projet par son identifiant.
@@ -29,7 +36,10 @@ interface UserDaoInterface
      * 
      * @return bool Comfirmation de connection
      */
-    public function SignIn(string $login, string $password): string;
+    public function SignIn(string $login, string $password): string
+    {
+        return true;
+    }
 
     /**¨
      * Change le mot de passe d'un Utilisateur.
@@ -39,7 +49,10 @@ interface UserDaoInterface
      * 
      * @return bool Confirmation de l'utilisateur
      */
-    public function ChangePassword(int $idUser, string $mdp): bool;
+    public function ChangePassword(int $idUser, string $mdp): bool
+    {
+        return true;
+    }
 
 
     /**
@@ -50,7 +63,10 @@ interface UserDaoInterface
      * @param $displayTips Affichage des conseil
      * @param $style style choisit
      */
-    public function ChangeSettings(int $idUser, ?bool $displayTips, ?int $style);
+    public function ChangeSettings(int $idUser, ?bool $displayTips, ?int $style)
+    {
+
+    }
 
     /**
      * Récupere un User liée au token donné
@@ -59,5 +75,8 @@ interface UserDaoInterface
      * 
      * @return User un User
      */
-    public function GetUserByToken(string $token) : User;
+    public function GetUserByToken(string $token) : User
+    {
+        return new User();
+    }
 }
