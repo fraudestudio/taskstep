9736110628095841493458995d9087cba9e1bb6d
AlexandreANSTETT <alexandre.anstett@iut-dijon.u-bourgogne.fr>
2023-04-24 12:16:43 +0200

    Implémentation de l'opération SignIn
    Implémentation de Sign In.
    Utilisation de deux nouvelles fonction:
    - Authentification : permettant de vérifier deux mots de passe.
    - CreateToken :
    Creer un token et le renvoie

--- Logic/Data/MySql/Dao/UserDao.php
+++ Logic/Data/MySql/Dao/UserDao.php
@@ -2,11 +2,13 @@
 
 namespace TaskStep\Data\MySql\Dao;
 
+use DateTime;
 use TaskStep\Data\Database;
 use TaskStep\Logic\Model;
 use TaskStep\Logic\Model\User;
 use TaskStep\Logic\Model\UserDaoInterface;
 use PDO;
+use Random\Randomizer;
 
 class UserDAO implements UserDaoInterface
 {
@@ -22,7 +24,7 @@ class UserDAO implements UserDaoInterface
      */ 
     public function SignUp(string $login, string $password, string $mail) : int
     {
-        $request = "Insert into `User`(login,MDP,salt,mail,style,tips) values (:login,:mdp,'SELTAMERE',:mail,0,1);";
+        $request = "Insert into `User`(login,MDP,salt,mail,style,tips) values (:login,:mdp,'',:mail,0,1);";
         Database::getInstance()->executeNonQuery($request,array('login'=>htmlspecialchars($login),'mdp'=>password_hash($password,PASSWORD_BCRYPT),'mail'=>$mail));
         $data = Database::getInstance()->executeQuery("select last_insert_id();")->fetch(PDO::FETCH_ASSOC);
         return $data['id'];
@@ -36,9 +38,44 @@ class UserDAO implements UserDaoInterface
      * 
      * @return bool Comfirmation de connection
      */
-    public function SignIn(string $login, string $password): string
+    public function SignIn(string $login, string $password): ?string
     {
-        return true;
+        $query = "Select id,MDP from User where login = :login";
+        $data = Database::getInstance()->executeQuery($query,array('login'=>$login))->fetch(PDO::FETCH_ASSOC);
+        if($this->Authentification($data['MDP'],$password)){
+            return $this->CreateToken($data['id']);
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * Vérifie si deux mots de passe sont les même
+     */
+    private function Authentification($expectedPass,$actualPass): bool{
+        return password_verify($actualPass,$expectedPass);
+    }
+
+    /**
+     * Créer un token, le rentre en base de données et le renvoie à l'utilisateur
+     * 
+     * @param $idUser Id de l'utilisateur concerné par ce token
+     */
+    private function CreateToken($idUser):string{
+        $query = "Insert into `Session` (Token,`date`,`User`) values (:token,:date,:idUser)";
+
+        //Création du Token
+        $tableau = "abcdefghijklmnopqrstuvwxyz";
+        $token = "";
+        for($i = 0 ; $i<10; $i++){
+            $token += $tableau[random_int(0,26)];
+        }
+        //Création de la date
+        $date = new DateTime();
+
+        Database::getInstance()->executeQuery($query,array('token'=> $token,'date'=>$date,'idUser'=>$idUser));
+
+        return $token;
     }
 
     /**¨
