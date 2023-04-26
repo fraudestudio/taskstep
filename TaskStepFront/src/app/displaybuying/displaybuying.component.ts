e1c801f8fc3819d01564e2931ca9bcfde9b4e6be
Pires "Indorie" Lucas <94078903+IndorieSama@users.noreply.github.com>
2023-04-26 14:47:56 +0200

    Commit Base Display of Item

---
+++ TaskStepFront/src/app/displaybuying/displaybuying.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displaybuying',
+  templateUrl: './displaybuying.component.html',
+})
+export class DisplaybuyingComponent {
+
+}
