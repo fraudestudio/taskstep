ff0ad56a0b8d9f38f05d35562b129a69071696ea
Louis DEVIE <louisdevie.contact@gmail.com>
2023-05-01 19:39:03 +0200

    redirection quand arrive sur le site

--- includes/sessioncheck.php
+++ includes/sessioncheck.php
@@ -5,13 +5,21 @@ require_once "autoload.php";
 session_start();  
 header("Cache-control: private");
 
-if(!$_SESSION['loggedin'])
+if(!$_SESSION['loggedin'] ?? false)
 {
 	$host  = $_SERVER['HTTP_HOST'];
 	$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/');
 	$then = rawurlencode(ltrim($_SERVER['REQUEST_URI'], '/'));
 	session_write_close();
-	header("Location: http://$host$baseUri/login.php?then=$then");
+	if ($then === '')
+	{
+		// redirection ver l'app angular si l'utilisateur arrive sur le site
+		header("Location: http://$host$baseUri/app/login");
+	}
+	else
+	{
+		header("Location: http://$host$baseUri/login.php?then=$then");
+	}
 	exit;
 }
 
